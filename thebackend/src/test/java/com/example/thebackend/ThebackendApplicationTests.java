e937b39c8e5d955d893ffcf37a1ed52381b63633
HPME <443808626@qq.com>
2022-03-07 18:56:56 +0800

    3.7

---
+++ thebackend/src/test/java/com/example/thebackend/ThebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
